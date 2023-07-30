d3edfea0a941dd60caf6ce311fd729110bb7e1c9
Dmitry <dm93vtb@gmail.com>
2023-07-30 22:04:21 +0300

    new

--- Example/js/script.js
+++ Example/js/script.js
@@ -3,9 +3,13 @@ const menuMobile = document.querySelector('.header__menu-list');
 
 menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('open');
+    document.querySelector('.header__icon').style.display = 'none';
+    document.querySelector('.menu__btn').style.display = 'none';
 });
 
 const menuBtnClose = document.querySelector('.menu__btn-close');
 menuBtnClose.addEventListener('click', () => {
     menuMobile.classList.remove('open');
+    document.querySelector('.header__icon').style.display = 'block';
+    document.querySelector('.menu__btn').style.display = 'block';
 });
