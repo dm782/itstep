519c7472233533f63ad0cf8aa186936cb347fa40
Dmitry <dm93vtb@gmail.com>
2023-09-10 12:16:41 +0300

    new

---
+++ Calendar Web-App/js/script.js
@@ -0,0 +1,5 @@
+let day1 = document.querySelector('.day1');
+day1.addEventListener('click', function () {
+    let input = document.createElement('input');
+    document.body.appendChild(input);
+});
